051addf0f662e8204753e16e80769753531001bf
omrawaley <omrawaley@icloud.com>
2025-01-16 20:10:31 -0500

    Fix missing EmptyFrame tiles

--- src/level.cpp
+++ src/level.cpp
@@ -4,7 +4,7 @@ void Level::resize(SDL_Renderer* renderer, int width, int height)
 {
     this->data.clear();
 
-    SDL_Texture* frameTexture = IMG_LoadTexture(renderer, "../assets/EmptyFrame.png");
+    SDL_Texture* frameTexture = IMG_LoadTexture(renderer, "assets/EmptyFrame.png");
 
     for(size_t row = 0; row < height; ++row)
     {
